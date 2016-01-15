c94b69a353a8598639f95393f86654bbaea77e1f
DZ <rdavidzhong@gmail.com>
2016-01-15 15:50:26 -0800

    Sync

---
+++ simple/simple/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  simple
+//
+//  Created by David Zhong on 2016-01-15.
+//  Copyright © 2016 David Zhong. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Overwrite
+    
+    int t = 12;
+    
+    cout << t << endl;
+    
+    t = 30;
+    
+    cout << t << endl;
+    
+    // Basic arithmetic
+    int x = 30 % 7;
+    cout << x << endl;
+    
+    return 0;
+}
