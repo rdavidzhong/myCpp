a3bb7cd91679a972cc433d53f7ed5325995a14fa
DZ <rdavidzhong@gmail.com>
2016-01-20 01:54:12 -0800

    Sync

---
+++ const/const/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  const
+//
+//  Created by David Zhong on 2016-01-19.
+//  Copyright © 2016 David Zhong. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    const int x = 3;
+    
+    x = 5;
+    cout << x << endl;
+}
